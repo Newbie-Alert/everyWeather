8ae1d3b2a8b3c6d2d89c607d4e31d492633e900e
NebieAlert <mjnn100801@naver.com>
2023-11-03 20:35:04 +0900

    주석 추가

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import styles from "./Forecast.module.css";
 
 export default function Forecast({ forecastData }) {
-  console.log(forecastData);
   const utc = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000;
   const krDiff = 9 * 60 * 60 * 1000;
   const koreaDate =
     forecastData?.city.country === "KR" ? new Date(utc + krDiff) : new Date();
 
+  // 한국 기준 년-월-일
   const todayKorea = `${koreaDate.getUTCFullYear()}-${
     koreaDate.getUTCMonth() + 1
   }-${String(koreaDate.getUTCDate()).padStart(2, "0")}`;
 
+  // 한국 기준 년-월-일을 포함한 데이터를 가져옵니다.
   const todayForeCast = forecastData?.list.filter((data) =>
     data.dt_txt.includes(todayKorea)
   );
@@ -20,7 +21,7 @@ export default function Forecast({ forecastData }) {
     <div className={styles.forecast_container}>
       {todayForeCast?.map((el) => {
         return (
-          <div className={styles.today_forecast}>
+          <div key={el.dt} className={styles.today_forecast}>
             <div className={styles.forecast_img}>
               <img
                 src={`https://openweathermap.org/img/wn/${el.weather[0].icon}.png`}
